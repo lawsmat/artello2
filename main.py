d4303c199e15c0e7e0d5e2b2aafd7dd1f5937fea
MoonBarc <no@lawsmat.com>
2021-04-18 20:22:40 -0400

    travel distance

--- main.py
+++ main.py
@@ -13,7 +13,18 @@ april = pupil_apriltags.Detector(
     debug=0
 )
 
+# AprilTag size in meters
 TAG_SIZE = 0.2
+# Frames to go bye until mission is deemed "failure" and lands
+FAILURE_THRESHOLD = 10
+# distance to travel before adjusting
+ADJ_DISTANCE = 300
+
+# random placeholder value until actually set
+tag_distance = 30000
+distance_phase = 1
+failure_count = 0
+travelling = False
 
 drone.connect()
 drone.streamon()
@@ -40,6 +51,9 @@ f.close()
 
 def main():
     while True:
+            global failure_count
+            global travelling
+            global distance_phase
             frame = drone.get_frame_read().frame
             gray = None
             try:
@@ -48,10 +62,22 @@ def main():
                 print("[!] No frame!") 
             tags = april.detect(gray, estimate_tag_pose=True, camera_params=[data['fx'], data['fy'], data['cx'], data['cy']], tag_size=TAG_SIZE)
             handled = False
+            if travelling and len(tags) == 0:
+                failure_count += 1
+            else:
+                failure_count = 0
+            if failure_count >= 10:
+                print("FAILURE!!!")
+                failure_count = 0
+                #stop
+                drone.send_rc_control(0,0,0,0)
+                #land
+                drone.land()
+                break
             for tag in tags:
-                distance = tag.pose_t[0] + tag.pose_t[1] + tag.pose_t[2]
-                print("Distance from tag #" + str(tag.tag_id) + ": " + str(distance) + " meters!")
-                look_at(tag.pose_t)
+                # in cm
+                pnt = tag.pose_t
+                distance = int(math.sqrt(math.pow(pnt[0],2) + math.pow(pnt[2],2)) * 100)
                 (ptA, ptB, ptC, ptD) = tags[0].corners
                 ptB = (int(ptB[0]), int(ptB[1]))
                 ptC = (int(ptC[0]), int(ptC[1]))
@@ -62,12 +88,27 @@ def main():
                 cv2.line(frame, ptB, ptC, (0, 255, 0), 2)
                 cv2.line(frame, ptC, ptD, (0, 255, 0), 2)
                 cv2.line(frame, ptD, ptA, (0, 255, 0), 2)
-                frame = cv2.putText(frame, str(round(distance[0], 2)), (int(tag.center[0]), int(tag.center[1])), cv2.FONT_HERSHEY_SIMPLEX, 1, (150,0,0), 2, cv2.LINE_AA)
+                frame = cv2.putText(frame, str(round(distance, 2)), (int(tag.center[0]), int(tag.center[1])), cv2.FONT_HERSHEY_SIMPLEX, 1, (150,0,0), 2, cv2.LINE_AA)
                 key = cv2.waitKey(10) & 0xFF
                 handled = True
                 if handleKey(key, pnt=tag.pose_t):
                     print("Closing!")
                     break
+                if ADJ_DISTANCE * distance_phase > distance and travelling:
+                    print("Adjusting")
+                    #stop drone
+                    drone.send_rc_control(0,0,0,0)
+                    # look at pose
+                    detect_rotate(pnt=tag.pose_t)
+                    # start
+                    drone.send_rc_control(0,50,0,0)
+                    handle_height(tag.pose_t)
+                    distance_phase += 1
+                if distance < 350 and travelling:
+                    print("Stopping, arrived!")
+                    #stop drone
+                    drone.send_rc_control(0,0,0,0)
+                    travelling = False
             cv2.imshow("drone", frame)
             # if len(tags) == 0:
             #     cv2.imshow("drone", frame)
@@ -90,6 +131,16 @@ def look_at(point):
 
     return rotx
 
+def handle_height(pnt):
+    distance = pnt[1]
+    if abs(distance) < 0.1: # 10 cm
+        return
+    else:
+        if distance > 0:
+            drone.move_up(distance)
+        else:
+            drone.move_down(-distance)
+
 def handleKey(key, pnt=None):
     if key == ord("q"):
         return True
@@ -102,22 +153,35 @@ def handleKey(key, pnt=None):
             drone.land()
         threading.Thread(target=land).start()
     if key == ord("y"):
-        # look at
         if pnt is None:
             print("NO TAG!")
             return
-        angle = look_at(pnt)
-        if(angle > 0):
-            # cw
-            def cw(angle, drone):
-                drone.rotate_clockwise(int(angle))
-            threading.Thread(target=cw, args=(angle, drone)).start()
-        else:
-            # ccw
-            def ccw(angle, drone):
-                drone.rotate_counter_clockwise(int(-angle))
-            threading.Thread(target=ccw, args=(angle, drone)).start()
+        threading.Thread(target=travel, args=(pnt, )).start()
+    if key == ord("x"):
+        # STOP!!!
+        drone.send_rc_control(0,0,0,0)
 
+def detect_rotate(pnt):
+    # look at
+    if pnt is None:
+        print("NO TAG!")
+        return
+    angle = look_at(pnt)
+    if angle < 5:
+        return
+    if(angle > 0):
+        # cw
+        drone.rotate_clockwise(int(angle))
+    else:
+        # ccw
+        drone.rotate_counter_clockwise(int(-angle))
+
+def travel(pnt): 
+    detect_rotate(pnt)
+    global travelling
+    if not travelling:
+        travelling = True
+        drone.send_rc_control(0,50,0,0)
 
 
 if __name__ == "__main__":
